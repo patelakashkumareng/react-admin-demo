f0f58e07003085b74381af48f0e7e7f6409e967e
Kishan Sakariya <kishan@nxgnsportsinteractive.com>
2024-01-29 18:10:33 +0530

    logo added in login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -55,13 +55,16 @@ export default function Login() {
           <div className="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
             <div className="d-table-cell align-middle">
               <div className="text-center mt-4">
-                <h1 className="h2">Welcome back</h1>
+                <h1 className="h2">Welcome back, Twelfthman</h1>
                 <p className="lead">Sign in to your account to continue</p>
               </div>
 
               <div className="card">
                 <div className="card-body">
                   <div className="m-sm-4">
+                  <div className="text-center">
+										<img src="img\brands\logo.svg" alt="Twelfthman" className={"img-fluid"} style={{width:"125px"}} />
+									</div>
                     {isLoading && <Loading />}
                     <form onSubmit={handleSubmit(submitHandler)}>
                       <Input
